20750c92cc9cf45b950e6ea065b69f1f71f0be3f
KMohnishM <156217686+KMohnishM@users.noreply.github.com>
2024-05-23 11:57:30 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b69fe4f155mshd6e74a36a45441dp1b3de2jsnecdcbd669798',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+  export const WEATHER_API_URL="https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY="2d5fe307770ac523956740a703bb03bc";
+  
